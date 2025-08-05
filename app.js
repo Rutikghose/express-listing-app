45480a298dcb333d374edd51f5d0d0ca4e89df27
Rutik Ghose <your_github_email@example.com>
2025-08-06 02:47:04 +0530

    Fix session config for Render deployment

--- app.js
+++ app.js
@@ -58,7 +58,9 @@ const sessionConfig = {
     resave: false,
     saveUninitialized: true,
     cookie: {
-        httpOnly: true,
+         httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax", 
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
